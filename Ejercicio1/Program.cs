f1bc29ac1db73c89b2142b9b99f8729a58261ae4
Gonzalo Sebastian Mendez <gonzams10@gmail.com>
2023-04-19 12:40:18 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var personas = new List<PersonaEnt>();
+while (true)
+{
+    var nuevaPersona = IngresarPersona();
+    personas.Add(nuevaPersona);
+    Console.WriteLine("Se ha agregado una nueva persona.");
+    Console.WriteLine($"Cantidad: {personas.Count}");
+
+    Console.WriteLine("Desea continuar[s/n]");
+    string continuar = null;
+    while (continuar != "S" && continuar != "N")
+    {
+        continuar = Console.ReadLine();
+    }
+    if (continuar == "N")
+    {
+        break;
+    }
+
+   
+
+}
+
+class PersonaEnt
+{
+
+}
+
+
+
+
